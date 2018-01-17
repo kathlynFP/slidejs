c6acbd31d34ec9cd88c8dddfa4ee3222b7c5179b
kathlyn <kathlyn.ferreira@negocie.online>
2018-01-17 16:02:29 -0200

    deixando slide responsivo

--- js/slide.js
+++ js/slide.js
@@ -3,17 +3,22 @@ function Slide(content, prev, next){
   this.margin = 0;
   this.prev = prev;
   this.next = next;
+  this.items = this.content.children;
+  this.conter = 0;
+  this.lastItem = this.items[0];
   this.bindEvents = function(){
     var _this = this;
     window.setInterval(function(){
       this.changeImg(true);
     }.bind(this), 2000);
     
-    this.prev.addEventListener("click", function(){
+    this.prev.addEventListener("click", function(){     
       _this.changeImg(false);
+      
     });
      
     this.next.addEventListener("click", function(){
+      console.log(_this.conter);
       _this.changeImg(true);
     });
 
@@ -28,37 +33,36 @@ Slide.prototype.changeImg = function(changeNext){
   else{
    this.prevImg();
   }
+  this.lastItem.classList.remove("item-active");
+  this.lastItem = this.items[this.conter];
+
+  this.items[this.conter].classList.add("item-active");
+
 }
 Slide.prototype.nextImg = function(){
-  if(this.margin < (this.content.children.length -1) * 1000){
-    this.margin = this.margin + 1000;
-    console.log(this.margin);
-   }    
-   else{
-    this.margin = 0;
-    console.log(this.margin);
-   }
-  
-  this.content.style.marginLeft = "-"+this.margin+"px";
+  if(this.conter < this.items.length - 1)
+    this.conter++;
+  else if(this.conter > -1)
+    this.conter = 0;
+   
 }
 Slide.prototype.prevImg = function(){
-  if(this.margin > 0)
-    this.margin = this.margin - 1000;
-  else
-    this.margin = 2000;
+  if(this.conter > 0)
+    this.conter--;
+  else 
+    this.conter = this.items.length - 1;
 
-  this.content.style.marginLeft = "-"+this.margin+"px";
 }
 
 var prev = document.getElementById("btn-prev"),
     next = document.getElementById("btn-next"),
-    content = document.getElementsByClassName("box-slide")[0];
+    content = document.getElementById("slide");
 
 var slide1 = new Slide(content, prev, next);
 
 
 var prev2 = document.getElementById("btn-prev2"),
-next2 = document.getElementById("btn-next2"),
-content2 = document.getElementsByClassName("box-slide2")[0];
+    next2 = document.getElementById("btn-next2"),
+    content2 = document.getElementById("slide2");
 
 var slide2 = new Slide(content2, prev2, next2);
